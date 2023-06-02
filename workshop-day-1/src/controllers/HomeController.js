500338ebf86fc6d305d5c4edb4de7ba8796482be
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-06-02 15:35:29 +0545

    crud using postgres and node

--- workshop-day-1/src/controllers/HomeController.js
+++ workshop-day-1/src/controllers/HomeController.js
@@ -1,10 +1,10 @@
 module.exports = {
-  const: (users = [
+  users: [
     { name: "John", email: "john@example.com", role: "Frontend Dev" },
     { name: "Aron", email: "ar@outlook.com", role: " Devops" },
     { name: "Leo", email: "leo@gmail.com", role: "DB Engineer" },
     { name: "Psy", email: "psy@yahoo.com", role: "UI/ UX " },
-  ]),
+  ],
   index: (req, res) => {
     res.render("index", { users });
   },
