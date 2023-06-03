0ba72b01c30cd743e51c4bc2837e96585a8bf04e
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-06-03 07:06:56 +0545

    fix git error

--- day-2-crud-postgres/sequelize-config.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("workshop_db", "postgres", "admin", {
-  host: "localhost",
-  dialect: "postgres",
-});
-
-module.exports = sequelize;
--- day-2-crud-postgres/src/controllers/HomeController.js
+++
@@ -1,7 +0,0 @@
-const User = require("@/models/UserModel");
-module.exports = {
-  index: async (req, res) => {
-    const list = await User.findAll();
-    res.send(list);
-  },
-};
